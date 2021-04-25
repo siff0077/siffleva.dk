511235e65f3dbc27d24010800aa77e731f7d17dd
siff0077 <61138080+siff0077@users.noreply.github.com>
2021-04-26 00:16:43 +0200

    should be responsive until 1900 px - working on animations and scrolltrigger

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+window.addEventListener('load', fade_in);
+
+function fade_in(){ 
+    /* document.querySelector("#splash_p").classList.add("fade_in"); */
+
+    /* gsap.registerPlugin(ScrollTrigger);
+  gsap.to('progress', {
+    value: 100,
+    ease: 'none',
+    scrollTrigger: { scrub: 0.3 }
+  });
+
+
+
+  gsap.registerPlugin(ScrollTrigger);
+        gsap.to("section",{x: "10vw", scrollTrigger:{trigger:"#box1",scrub:true}}); */
+        
+
+}
