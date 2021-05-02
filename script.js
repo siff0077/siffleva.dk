4204c9f2c07da68f5e413a50d02440e0303ef1eb
siff0077 <61138080+siff0077@users.noreply.github.com>
2021-05-02 15:49:19 +0200

    scrolltrigger added for frontpage

--- script.js
+++ script.js
@@ -1,21 +1,80 @@
-"use strict"
-
-window.addEventListener('load', fade_in);
-
-function fade_in(){ 
-    /* document.querySelector("#splash_p").classList.add("fade_in"); */
-
-    /* gsap.registerPlugin(ScrollTrigger);
-  gsap.to('progress', {
-    value: 100,
-    ease: 'none',
-    scrollTrigger: { scrub: 0.3 }
-  });
+"use strict";
 
 
+window.addEventListener("DOMContentLoaded", init);
+gsap.registerPlugin(ScrollTrigger);
 
-  gsap.registerPlugin(ScrollTrigger);
-        gsap.to("section",{x: "10vw", scrollTrigger:{trigger:"#box1",scrub:true}}); */
-        
-
+function init() {
+  fadeIn();
 }
+
+function fadeIn() {
+  // Make section fade in from the bottom
+  gsap.to("#about", {
+    scrollTrigger: "#about",
+    duration: 1,
+    opacity: 1,
+    y: 0,
+    onLeave: () => gsap.to("#about", {
+      y: -50,
+      opacity: 0,
+      stagger: 0.2,
+    }),
+    onEnterBack: () => gsap.to("#about", {
+      y: 0,
+      opacity: 1,
+      stagger: -0.2,
+    }),
+    onLeaveBack: () => gsap.to("#about", {
+      y: 50,
+      opacity: 0,
+      stagger: -0.2,
+    }),
+  });
+  
+  gsap.to("#resume", {
+    scrollTrigger: "#resume",
+    duration: 1,
+    opacity: 1,
+    y: 0,
+    onLeave: () => gsap.to("#resume", {
+      y: -50,
+      opacity: 0,
+      stagger: 0.2,
+    }),
+    onEnterBack: () => gsap.to("#resume", {
+      y: 0,
+      opacity: 1,
+      stagger: -0.2,
+    }),
+    onLeaveBack: () => gsap.to("#resume", {
+      y: 50,
+      opacity: 0,
+      stagger: -0.2,
+    }),
+  }); 
+
+  gsap.to("#skills", {
+    scrollTrigger: "#skills",
+    duration: 1,
+    opacity: 1,
+    y: 0,
+    onLeave: () => gsap.to("#skills", {
+      y: -50,
+      opacity: 0,
+      stagger: 0.2,
+    }),
+    onEnterBack: () => gsap.to("#skills", {
+      y: 0,
+      opacity: 1,
+      stagger: -0.2,
+    }),
+    onLeaveBack: () => gsap.to("#skills", {
+      y: 50,
+      opacity: 0,
+      stagger: -0.2,
+    }),
+  }); 
+}
+
+
