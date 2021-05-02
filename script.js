7891110b25b4b06ae5153c654b2ab123e53ed2f1
siff0077 <61138080+siff0077@users.noreply.github.com>
2021-05-02 16:32:11 +0200

    changes to scrolltrigger

--- script.js
+++ script.js
@@ -12,68 +12,23 @@ function fadeIn() {
   // Make section fade in from the bottom
   gsap.to("#about", {
     scrollTrigger: "#about",
-    duration: 1,
+    duration: 1.5,
     opacity: 1,
     y: 0,
-    onLeave: () => gsap.to("#about", {
-      y: -50,
-      opacity: 0,
-      stagger: 0.2,
-    }),
-    onEnterBack: () => gsap.to("#about", {
-      y: 0,
-      opacity: 1,
-      stagger: -0.2,
-    }),
-    onLeaveBack: () => gsap.to("#about", {
-      y: 50,
-      opacity: 0,
-      stagger: -0.2,
-    }),
   });
   
   gsap.to("#resume", {
     scrollTrigger: "#resume",
-    duration: 1,
+    duration: 1.5,
     opacity: 1,
     y: 0,
-    onLeave: () => gsap.to("#resume", {
-      y: -50,
-      opacity: 0,
-      stagger: 0.2,
-    }),
-    onEnterBack: () => gsap.to("#resume", {
-      y: 0,
-      opacity: 1,
-      stagger: -0.2,
-    }),
-    onLeaveBack: () => gsap.to("#resume", {
-      y: 50,
-      opacity: 0,
-      stagger: -0.2,
-    }),
   }); 
 
   gsap.to("#skills", {
     scrollTrigger: "#skills",
-    duration: 1,
+    duration: 1.5,
     opacity: 1,
     y: 0,
-    onLeave: () => gsap.to("#skills", {
-      y: -50,
-      opacity: 0,
-      stagger: 0.2,
-    }),
-    onEnterBack: () => gsap.to("#skills", {
-      y: 0,
-      opacity: 1,
-      stagger: -0.2,
-    }),
-    onLeaveBack: () => gsap.to("#skills", {
-      y: 50,
-      opacity: 0,
-      stagger: -0.2,
-    }),
   }); 
 }
 
